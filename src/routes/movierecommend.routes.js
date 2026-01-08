da74e55294fb6e91f546c001e1a60a6b2d3abff0
012ZulkarNain <012zulkarnain@gmail.com>
2026-01-08 10:06:52 +0530

    fix double api prefix final

--- src/routes/movierecommend.routes.js
+++ src/routes/movierecommend.routes.js
@@ -1,7 +1,5 @@
 const { recommendMovies } = require("../controllers/movierecommend.controller");
 
-async function routes(fastify, options) {
-  fastify.post("/api/recommend", recommendMovies);
-}
-
-module.exports = routes;
+module.exports = async function (fastify) {
+  fastify.post("/recommend", recommendMovies);
+};
